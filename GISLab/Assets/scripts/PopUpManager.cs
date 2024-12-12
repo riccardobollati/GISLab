2e971de55f5cc393f39b2a58de02af908fb26a03
Riccardo <riccardo.bollati.01@gmail.com>
2024-12-12 16:45:08 +0100

    image

--- GISLab/Assets/scripts/PopUpManager.cs
+++ GISLab/Assets/scripts/PopUpManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using TMPro;
 
 public class PopUpManager : MonoBehaviour
@@ -14,6 +15,7 @@ public class PopUpManager : MonoBehaviour
     TMP_Text description;
     TMP_Text idText;
     TMP_Text timeObservedAt;
+    Renderer ImageRenderer;
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +27,14 @@ public class PopUpManager : MonoBehaviour
     {
 
         // get text component
-        Debug.Log(transform.Find("content/Info/common_name"));
         commonName = transform.Find("content/Info/common_name").GetComponent<TMP_Text>();
-        Debug.Log(commonName);
         scientificName = transform.Find("content/Info/scientific_name").GetComponent<TMP_Text>();
         iconicTaxonName = transform.Find("content/Info/iconic_taxon_name").GetComponent<TMP_Text>();
         description = transform.Find("content/Info/description").GetComponent<TMP_Text>();
         idText = transform.Find("content/Info/id").GetComponent<TMP_Text>();
         timeObservedAt = transform.Find("content/time_observed_at").GetComponent<TMP_Text>();
+        ImageRenderer = transform.Find("content/image").GetComponent<Renderer>();
+
 
         // get observation
         Dictionary<string, string> observation = db.getObservationByID(id);
@@ -41,10 +43,45 @@ public class PopUpManager : MonoBehaviour
         commonName.text = observation["common_name"];
         scientificName.text = observation["scientific_name"];
         iconicTaxonName.text = observation["iconic_taxon_name"];
-        description.text = observation["description"];
+        if(observation["description"] == "")
+            description.text = "No description";
+        else
+            description.text = observation["description"];
+            
         idText.text = observation["id"];
-        timeObservedAt.text = observation["time_observed_at"];
+        if(observation["time_observed_at"] == "")
+            timeObservedAt.text = "No date found";
+        else
+            timeObservedAt.text = observation["time_observed_at"];
+        
+        // get image
+        StartCoroutine(DownloadAndSetImage(observation["image_url"]));
     }
+    private IEnumerator DownloadAndSetImage(string imageUrl)
+    {
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl))
+        {
+            Debug.Log("Getting image: " + imageUrl);
+
+            // Send the request and wait for the download to complete
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                // Get the downloaded texture
+                Texture2D downloadedTexture = DownloadHandlerTexture.GetContent(request);
+
+                // Apply the texture to the material
+                ImageRenderer.material.SetTexture("_MainTex", downloadedTexture);
+                Debug.Log("Texture successfully applied from URL.");
+            }
+            else
+            {
+                Debug.LogError("Failed to download texture: " + request.error);
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
